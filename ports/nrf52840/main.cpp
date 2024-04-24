58f842625779329f54119e4ece03bc7d2ee52cda
Tooww <tom.leizerovici@edu.devinci.fr>
2024-04-24 15:24:11 +0200

    fix main

--- ports/nrf52840/main.cpp
+++ ports/nrf52840/main.cpp
@@ -604,8 +604,7 @@ int main()
 
 	DEBUG_TRACE("BARO...");
 	try {
-		ads1015LL ads1015Instance(ADC_BAROMETER_DEVICE,ADC_BAROMETER_ADDR,BAROMETER_SLEEP_EN);
-		static ads1015 baro(ads1015Instance);
+		static ads1015 baro;
 		static BAROSensorService baro_sensor_service(baro, &baro_sensor_log);
 	} catch (...) {
 		DEBUG_TRACE("BARO: not detected");
