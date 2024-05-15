566a7c475a46d405d64bef69f7b210f232678a99
Tooww <tom.leizer@gmail.com>
2024-05-15 11:02:23 +0200

    debug physical test #3

--- ports/nrf52840/core/hardware/ads1015/ads1015.cpp
+++ ports/nrf52840/core/hardware/ads1015/ads1015.cpp
@@ -30,22 +30,24 @@ void ads1015LL::send_command(uint8_t command)
 
 void ads1015LL::send_command_conf(uint8_t *command)
 {
+  DEBUG_TRACE("ads1015LL::send_command_conf");
 	NrfI2C::write(m_bus, m_addr, command, 3, false);
 }
 
 
-void ads1015LL::read(int& digital_value) {
+void ads1015LL::read(double& digital_value) {
     //start trigger
     //code gpio trig ;
     //int flag = 1;
 
-    DEBUG_TRACE("BAROMETER_SLEEP_EN is set to HIGH");
-     GPIOPins::set(BAROMETER_SLEEP_EN);
+    //DEBUG_TRACE("BAROMETER_SLEEP_EN is set to HIGH");
+     //GPIOPins::set(BAROMETER_SLEEP_EN);
      
 
     DEBUG_TRACE("Flag value before if statement: %d", flag);
     if (flag==1)
     {
+      flag=0;
       uint8_t write_buffer[3] = {ads1015Command::CONFIG_REG,ads1015Command::MSB_CONFIG,ads1015Command::LSB_CONFIG};
       send_command_conf(write_buffer);
       PMU::delay_ms(10);
@@ -71,6 +73,7 @@ uint16_t ads1015LL::sample_adc(uint8_t measurement) {
     PMU::delay_ms(10);
     uint8_t read_buffer[2];
     NrfI2C::read(m_bus,m_addr,read_buffer,2);
+    DEBUG_TRACE("ads1015LL::sample_adc");
     return ((uint16_t)read_buffer[0] << 8 ) + ((uint16_t)read_buffer[1]);
     
 }
