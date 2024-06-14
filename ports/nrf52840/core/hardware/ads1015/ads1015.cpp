cadce1159152b6fdf2fdc0af09f610600824d549
Tooww <tom.leizer@gmail.com>
2024-06-14 14:08:11 +0200

    test pin wake up #1

--- ports/nrf52840/core/hardware/ads1015/ads1015.cpp
+++ ports/nrf52840/core/hardware/ads1015/ads1015.cpp
@@ -41,8 +41,12 @@ void ads1015LL::read(double& digital_value) {
     //code gpio trig ;
     //int flag = 1;
 
-    //DEBUG_TRACE("BAROMETER_SLEEP_EN is set to HIGH");
-     //GPIOPins::set(BAROMETER_SLEEP_EN);
+    DEBUG_TRACE("BAROMETER_SLEEP_EN is set to HIGH");
+    GPIOPins::set(BAROMETER_SLEEP_EN);
+    PMU::delay_ms(1000);
+    DEBUG_TRACE("BAROMETER_SLEEP_EN is set to LOW");
+     GPIOPins::clear(BAROMETER_SLEEP_EN);
+
      
 
     DEBUG_TRACE("Flag value before if statement: %d", flag);
