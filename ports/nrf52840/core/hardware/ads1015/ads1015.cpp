7fdd00b5a40b6d9e99dd7b977eb32ddd4cbd0493
Tooww <tom.leizer@gmail.com>
2024-06-24 16:40:33 +0200

    baro valu offset correction #1

--- ports/nrf52840/core/hardware/ads1015/ads1015.cpp
+++ ports/nrf52840/core/hardware/ads1015/ads1015.cpp
@@ -72,7 +72,8 @@ void ads1015LL::read(double& digital_value) {
     DEBUG_TRACE("ads1015LL::read: %u bin >> 4 digital value = %f ", bin_value,digital_value); 
     //digital_value = (double)digital_value; 
     DEBUG_TRACE("ads1015LL::read: %f digit", digital_value);
-    digital_value = (digital_value/1666)*5;
+    digital_value = digital_value*3.0F;
+    //digital_value = (digital_value/1666)*5;
     DEBUG_TRACE("ads1015LL::read: %f Volt", digital_value);
     digital_value = 800 +((1060-800)/5)*digital_value;
     DEBUG_TRACE("ads1015LL::read: %f hPa", digital_value);
